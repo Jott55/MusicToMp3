61934001310d8154369c885870a6e809b523938f
Jott55 <josuhbs@gmail.com>
2025-10-11 09:21:44 -0300

    functional progress bar

--- M2Mp3.py
+++ M2Mp3.py
@@ -7,7 +7,8 @@ from pathlib import Path
 
 from convert import MusicConvert
 
-
+from kivy.clock import Clock
+import threading
 class RootWidget(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -17,9 +18,18 @@ class RootWidget(Widget):
         self.MusicConvert.add_music_dir()
         subprocess.Popen(['xdg-open', Path.cwd().joinpath(self.MusicConvert.music_dir) ])
         return 0
+    
+    def set_progress_bar_max(self, max):
+        self.ids.convert_progress_bar.max = max 
+    def set_progress_bar_value(self, value):
+        self.ids.convert_progress_bar.value = value
+
     def convert_music(self):
-        self.MusicConvert.convert_musics_to_mp3()
-        subprocess.Popen(['xdg-open', Path.cwd().joinpath(self.MusicConvert.out_dir) ])
+        self.set_progress_bar_value(0)
+        def new_thread_conversion():
+            self.MusicConvert.convert_musics_to_mp3(self.set_progress_bar_value, self.set_progress_bar_max)
+            subprocess.Popen(['xdg-open', Path.cwd().joinpath(self.MusicConvert.out_dir) ])
+        threading.Thread(target=new_thread_conversion, daemon=True).start()
 
 class MainApp(App):
 
--- convert.py
+++ convert.py
@@ -32,7 +32,7 @@ class MusicConvert():
             print(self.LANG_CODE["directoryAlready"])
             
 
-    def convert_musics_to_mp3(self) -> int:
+    def convert_musics_to_mp3(self, files_completed_callback = None, files_quantity_callback = None ) -> int:
         if not self.music_dir.is_dir():
             print(self.LANG_CODE["plsAdd"])
 
@@ -48,15 +48,21 @@ class MusicConvert():
         }
         options = ' '.join(options_verbose.values())
         
+        files_completed = 0
 
         for root, dirs, files in os.walk(self.music_dir):
+            if (files_quantity_callback):
+                files_quantity_callback(len(files))
             for file in files:
                 extension_location = file.rfind('.')
                 if (extension_location != -1):
                     output_file_name = file[:extension_location]
                 else: output_file_name = file
                 print(output_file_name)
-
+        
                 input_path = os.path.join(root, file)
                 print(f"ffmpeg -i '{input_path}' " + options + f" '{os.path.join(self.out_dir, output_file_name)}.mp3'")
                 subprocess.Popen(f"ffmpeg -i '{input_path}' " + options + f" '{os.path.join(self.out_dir, output_file_name)}.mp3'", shell=True).wait()
+                files_completed+=1
+                if files_completed_callback:
+                    files_completed_callback(files_completed)
